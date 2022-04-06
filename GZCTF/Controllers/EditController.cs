30724ebc6eb1a8d2618c0967dc219d6303409943
GZTimeWalker <gztimewalker@163.com>
2022-04-06 20:51:58 +0800

    wip: add EditController

---
+++ GZCTF/Controllers/EditController.cs
@@ -0,0 +1,43 @@
+﻿using CTFServer.Middlewares;
+using CTFServer.Repositories.Interface;
+using CTFServer.Utils;
+using CTFServer.Models.Request.Edit;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CTFServer.Controllers;
+
+/// <summary>
+/// 数据修改交互接口
+/// </summary>
+[RequireAdmin]
+[ApiController]
+[Route("api/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[ProducesResponseType(typeof(RequestResponse), StatusCodes.Status401Unauthorized)]
+[ProducesResponseType(typeof(RequestResponse), StatusCodes.Status403Forbidden)]
+public class EditController : Controller
+{
+    private readonly INoticeRepository noticeRepository;
+
+    public EditController(INoticeRepository _noticeRepository)
+    {
+        noticeRepository = _noticeRepository;
+    }
+
+    /// <summary>
+    /// 添加公告
+    /// </summary>
+    /// <remarks>
+    /// 添加公告，需要管理员权限
+    /// </remarks>
+    /// <param name="model"></param>
+    /// <response code="200">成功获取文件</response>
+    /// <response code="404">文件未找到</response>
+    [HttpGet("Notice")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public Task<IActionResult> AddNotice(NoticeModel model, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
