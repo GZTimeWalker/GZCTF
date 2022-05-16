2d3ffc1856f3fc51cefbde3e041bdfaf409058f5
GZTimeWalker <gztimewalker@163.com>
2022-05-16 20:37:27 +0800

    wip: update

--- GZCTF/ClientApp/src/pages/account/profile.tsx
+++ GZCTF/ClientApp/src/pages/account/profile.tsx
@@ -1,14 +1,34 @@
 import type { NextPage } from 'next';
-import { Box, Stack, Group, Divider, Title, Text, Button, Input, Grid, Card, TextInput, Paper, Center, Textarea, Modal, PasswordInput, Avatar, Image, Indicator } from '@mantine/core';
-import { Dropzone, DropzoneStatus, IMAGE_MIME_TYPE } from '@mantine/dropzone';
-import api from '../../Api';
-import WithNavBar from '../../components/WithNavbar';
 import { useState } from 'react';
+import {
+  Box,
+  Stack,
+  Group,
+  Divider,
+  Title,
+  Text,
+  Button,
+  Input,
+  Grid,
+  Card,
+  TextInput,
+  Paper,
+  Center,
+  Textarea,
+  Modal,
+  PasswordInput,
+  Avatar,
+  Image,
+  Indicator,
+  Header,
+} from '@mantine/core';
+import { Dropzone, DropzoneStatus, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import { useInputState } from '@mantine/hooks';
 import { showNotification } from '@mantine/notifications';
-import Icon from '@mdi/react';
 import { mdiAccessPoint, mdiAirHorn, mdiCheck, mdiClose } from '@mdi/js';
-
+import Icon from '@mdi/react';
+import api from '../../Api';
+import WithNavBar from '../../components/WithNavbar';
 
 const Profile: NextPage = () => {
   const [opened, setOpened] = useState(false);
@@ -18,18 +38,16 @@ const Profile: NextPage = () => {
   const [uname, setUname] = useInputState('');
   const [bio, setBio] = useInputState('');
   const [pwd, setPwd] = useInputState('');
-  const [avatar, setAvatar] = useState(data?.avatar||'');
+  const [avatar, setAvatar] = useState(data?.avatar || '');
   const [email, setEmail] = useInputState('');
 
   const onSaveChange = () => {
     setDisabled(true);
     if (email && email != data?.email) {
       setOpened(true);
-    }
-    else if ((uname && uname != data?.userName) || (bio && bio != data?.bio)) {
-      changeInfo()
-    }
-    else {
+    } else if ((uname && uname != data?.userName) || (bio && bio != data?.bio)) {
+      changeInfo();
+    } else {
       showNotification({
         color: 'red',
         title: '你没有做任何修改',
@@ -38,19 +56,19 @@ const Profile: NextPage = () => {
         disallowClose: true,
       });
     }
-    setDisabled(false)
-  }
+    setDisabled(false);
+  };
 
   const onConfirmEmail = () => {
-    setPwdDisabled(true)
+    setPwdDisabled(true);
     // Need a check code API
-    setOpened(false)
-    changeEmailAndInfo()
-    setPwdDisabled(false)
-  }
+    setOpened(false);
+    changeEmailAndInfo();
+    setPwdDisabled(false);
+  };
 
   const onChangeAvatar = () => {
-  /*
+    /*
     TODO: Profile props, update with mutate, avatar with DropZone
     Refer: https://mantine.dev/others/dropzone/
   */
@@ -61,7 +79,7 @@ const Profile: NextPage = () => {
       icon: <Icon path={mdiAccessPoint} size={1} />,
       disallowClose: true,
     });
-  }
+  };
 
   const onClearInfo = () => {
     setDisabled(true);
@@ -71,15 +89,15 @@ const Profile: NextPage = () => {
     setEmail('');
     // setAvatar(data?.avatar);
 
-    setDisabled(false)
-  }
+    setDisabled(false);
+  };
 
   const changeInfo = () => {
     if (uname || bio) {
       api.account
-      .accountUpdate({
-        userName: uname||data?.userName,
-        bio: bio||data?.bio,
+        .accountUpdate({
+          userName: uname || data?.userName,
+          bio: bio || data?.bio,
         })
         .then(() => {
           showNotification({
@@ -89,8 +107,8 @@ const Profile: NextPage = () => {
             icon: <Icon path={mdiCheck} size={1} />,
             disallowClose: true,
           });
-          onClearInfo()
-          mutate({...data})
+          onClearInfo();
+          mutate({ ...data });
         })
         .catch((err) => {
           showNotification({
@@ -101,13 +119,13 @@ const Profile: NextPage = () => {
           });
         });
     }
-  }
+  };
 
   const changeEmailAndInfo = () => {
     if (email) {
       api.account
-      .accountChangeEmail({
-        newMail : email
+        .accountChangeEmail({
+          newMail: email,
         })
         .then(() => {
           showNotification({
@@ -117,7 +135,7 @@ const Profile: NextPage = () => {
             icon: <Icon path={mdiCheck} size={1} />,
             disallowClose: true,
           });
-          changeInfo()
+          changeInfo();
         })
         .catch((err) => {
           showNotification({
@@ -128,127 +146,135 @@ const Profile: NextPage = () => {
           });
         });
     }
-  }
-
+  };
 
   return (
-  <>
-    {/* OverLay */}
-    <Modal
-      opened={opened}
-      onClose={() => setOpened(false)}
-      title="您即将修改邮箱，请确认密码"
-    >
-    <PasswordInput
-      required
-      label="密码"
-      placeholder="P4ssW@rd"
-      style={{ width: '100%' }}
-      value={pwd}
-      disabled={pwdDisabled}
-      onChange={(event) => setPwd(event.currentTarget.value)}
-    />
-    <Box style={{ margin: "auto", marginTop: "30px" }}>
-      {
-      <Grid grow>
-        <Grid.Col span={8}>
-        <Button fullWidth variant="outline" onClick={onConfirmEmail}>
-          确认修改
-        </Button>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Button fullWidth color="red" variant="outline" onClick={() => setOpened(false)}>
-            取消修改
-          </Button>
-        </Grid.Col>
-      </Grid>
-      }
-    </Box>
-    </Modal>
+    <>
+      {/* OverLay */}
+      <Modal opened={opened} onClose={() => setOpened(false)} title="您即将修改邮箱，请确认密码">
+        <PasswordInput
+          required
+          label="密码"
+          placeholder="P4ssW@rd"
+          style={{ width: '100%' }}
+          value={pwd}
+          disabled={pwdDisabled}
+          onChange={(event) => setPwd(event.currentTarget.value)}
+        />
+        <Box style={{ margin: 'auto', marginTop: '30px' }}>
+          {
+            <Grid grow>
+              <Grid.Col span={8}>
+                <Button fullWidth variant="outline" onClick={onConfirmEmail}>
+                  确认修改
+                </Button>
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <Button fullWidth color="red" variant="outline" onClick={() => setOpened(false)}>
+                  取消修改
+                </Button>
+              </Grid.Col>
+            </Grid>
+          }
+        </Box>
+      </Modal>
 
-    {/* Main Page */}
-    <WithNavBar>
-      <Stack align="center">
-        <Paper style={{ width: "55%", padding: "5%", paddingTop: "2%", marginTop: "5%" }} shadow = "sm" p="lg" >
-          {/* Header */}
-          <h2 style={{ marginBottom: "5px" }}>
-            个人信息
-          </h2>
-          <Divider/>
-          <Grid style={{ marginTop: "15px" }}>
-            {/* User Info */}
-            <Grid.Col span={9}>
-              {/* Input and Info display */}
-              <Group position='apart' style={{ margin: "auto" }}>
-                <TextInput
-                  label="用户名"
-                  type="text"
-                  placeholder={data?.userName ?? "ctfer"}
-                  style={{ width: "200px" }}
-                  value={uname}
-                  disabled={disabled}
-                  onChange={(event) => setUname(event.currentTarget.value)}
-                />
-                <TextInput
-                  label="邮箱"
-                  type="email"
-                  placeholder={data?.email ?? "ctfer@GZ.cn"}
-                  style={{ width: "100%" }}
-                  value={email}
-                  disabled={disabled}
-                  onChange={(event) => setEmail(event.currentTarget.value)}
-                />
-                <Textarea
-                  label="描述"
-                  placeholder={data?.bio ?? "这个人很懒，什么都没有写"}
-                  value={bio}
-                  style={{ width: '100%' }}
-                  disabled={disabled}
-                  autosize
-                  minRows={2}
-                  maxRows={4}
-                  onChange={(event) => setBio(event.currentTarget.value)}
-                />
-              </Group>
+      {/* Main Page */}
+      <WithNavBar>
+        <Stack align="center">
+          <Paper
+            style={{ width: '55%', padding: '5%', paddingTop: '2%', marginTop: '5%' }}
+            shadow="sm"
+            p="lg"
+          >
+            {/* Header */}
+            <Box style={{ marginBottom: '5px' }}>
+              <Header height={36}>个人信息</Header>
+            </Box>
+            <Divider />
+            <Grid style={{ marginTop: '15px' }}>
+              {/* User Info */}
+              <Grid.Col span={9}>
+                {/* Input and Info display */}
+                <Group position="apart" style={{ margin: 'auto' }}>
+                  <TextInput
+                    label="用户名"
+                    type="text"
+                    placeholder={data?.userName ?? 'ctfer'}
+                    style={{ width: '200px' }}
+                    value={uname}
+                    disabled={disabled}
+                    onChange={(event) => setUname(event.currentTarget.value)}
+                  />
+                  <TextInput
+                    label="邮箱"
+                    type="email"
+                    placeholder={data?.email ?? 'ctfer@GZ.cn'}
+                    style={{ width: '100%' }}
+                    value={email}
+                    disabled={disabled}
+                    onChange={(event) => setEmail(event.currentTarget.value)}
+                  />
+                  <Textarea
+                    label="描述"
+                    placeholder={data?.bio ?? '这个人很懒，什么都没有写'}
+                    value={bio}
+                    style={{ width: '100%' }}
+                    disabled={disabled}
+                    autosize
+                    minRows={2}
+                    maxRows={4}
+                    onChange={(event) => setBio(event.currentTarget.value)}
+                  />
+                </Group>
 
-              {/* Button: save Changes or clear Changes */}
-              <Box style={{ margin: "auto", marginTop: "30px" }}>
-              {
-              <Grid grow>
-                <Grid.Col span={8}>
-                  <Button fullWidth variant="outline" disabled={disabled} onClick={onSaveChange}>
-                    保存变更
-                  </Button>
-                </Grid.Col>
-                <Grid.Col span={4}>
-                  <Button fullWidth color="red" variant="outline" disabled={disabled} onClick={onClearInfo}>
-                    清除变更
-                  </Button>
-                </Grid.Col>
-              </Grid>
-              }
-              </Box>
-            </Grid.Col>
+                {/* Button: save Changes or clear Changes */}
+                <Box style={{ margin: 'auto', marginTop: '30px' }}>
+                  {
+                    <Grid grow>
+                      <Grid.Col span={8}>
+                        <Button
+                          fullWidth
+                          variant="outline"
+                          disabled={disabled}
+                          onClick={onSaveChange}
+                        >
+                          保存变更
+                        </Button>
+                      </Grid.Col>
+                      <Grid.Col span={4}>
+                        <Button
+                          fullWidth
+                          color="red"
+                          variant="outline"
+                          disabled={disabled}
+                          onClick={onClearInfo}
+                        >
+                          清除变更
+                        </Button>
+                      </Grid.Col>
+                    </Grid>
+                  }
+                </Box>
+              </Grid.Col>
 
-            {/* User Avator */}
-            <Grid.Col span={3}>
-              <Text size="sm">
-                用户头像
-              </Text>
-              <Stack align="center" style={{ width: "100%" }}>
-                <Avatar
-                  style={{ borderRadius: "50%" }}
-                  size="xl"
-                  src={avatar ?? "https://mantine.dev/images/avatar.png"}
-                  onClick={onChangeAvatar}
-                />
-              </Stack>
-            </Grid.Col>
-          </Grid>
-        </Paper>
-      </Stack>
-    </WithNavBar>
-  </>
+              {/* User Avator */}
+              <Grid.Col span={3}>
+                <Text size="sm">用户头像</Text>
+                <Stack align="center" style={{ width: '100%' }}>
+                  <Avatar
+                    style={{ borderRadius: '50%' }}
+                    size="xl"
+                    src={avatar ?? 'https://mantine.dev/images/avatar.png'}
+                    onClick={onChangeAvatar}
+                  />
+                </Stack>
+              </Grid.Col>
+            </Grid>
+          </Paper>
+        </Stack>
+      </WithNavBar>
+    </>
   );
 };
 
