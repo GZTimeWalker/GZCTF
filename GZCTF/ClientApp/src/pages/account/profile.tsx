093dee7781b2e0a4719ffdf7c523d17e6bbf51af
Hanmur <1466046208@qq.com>
2022-07-16 21:33:04 +0800

    Fix mailbox styles

--- GZCTF/ClientApp/src/pages/account/profile.tsx
+++ GZCTF/ClientApp/src/pages/account/profile.tsx
@@ -195,7 +195,7 @@ const Profile: NextPage = () => {
               type="email"
               style={{ width: '100%' }}
               value={data?.email ?? 'ctfer@gzti.me'}
-              disabled={disabled}
+              disabled = {true}
               readOnly
             />
             <TextInput
