a38f9f98472292c7392a26296fc21613b1b542d6
GZTimeWalker <Time.GZ@outlook.com>
2022-07-20 12:25:08 +0800

    wip: navigate to 404

--- GZCTF/ClientApp/src/pages/[...all].tsx
+++ GZCTF/ClientApp/src/pages/[...all].tsx
@@ -1,9 +1,19 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { Center } from '@mantine/core'
 import WithNavBar from '../components/WithNavbar'
 import Icon404 from '../components/icon/404Icon'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const Error404: FC = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname !== '/404') {
+      navigate('/404')
+    }
+  }, [location])
+
   return (
     <WithNavBar>
       <Center style={{ height: 'calc(100vh - 32px)' }}>
