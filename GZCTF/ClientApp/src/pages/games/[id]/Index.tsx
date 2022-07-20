89f9cf9040dfe53cd558da7ac39193d1df8e02fb
GZTimeWalker <Time.GZ@outlook.com>
2022-07-20 12:18:42 +0800

    fix: incorrect routing

--- GZCTF/ClientApp/src/pages/games/[id]/Index.tsx
+++ GZCTF/ClientApp/src/pages/games/[id]/Index.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
 import { useParams } from 'react-router-dom'
 import { Stack, Text } from '@mantine/core'
-import LogoHeader from '../../components/LogoHeader'
-import WithNavBar from '../../components/WithNavbar'
+import LogoHeader from '../../../components/LogoHeader'
+import WithNavBar from '../../../components/WithNavbar'
 
 const GameDetail: FC = () => {
   const { id } = useParams()
