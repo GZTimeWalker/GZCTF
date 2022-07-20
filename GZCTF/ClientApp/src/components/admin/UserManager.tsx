d7c246e5f97afa3a78f840b836738939ecdf7c06
chenjunyu19 <1335546814@qq.com>
2022-07-20 21:41:11 +0800

    wip: paging and improvement of user manager

--- GZCTF/ClientApp/src/components/admin/UserManager.tsx
+++ GZCTF/ClientApp/src/components/admin/UserManager.tsx
@@ -1,5 +1,5 @@
-import { FC, useState } from 'react'
-import { Group, Stack, Table, Text, ActionIcon, Badge } from '@mantine/core'
+import { FC, useEffect, useState } from 'react'
+import { Group, Stack, Table, Text, ActionIcon, Badge, Paper, NumberInput } from '@mantine/core'
 import { useModals } from '@mantine/modals'
 import { showNotification } from '@mantine/notifications'
 import {
@@ -14,19 +14,20 @@ import Icon from '@mdi/react'
 import api, { UserInfoModel } from '../../Api'
 import UserEditModal from './edit/UserEditModal'
 
-const ITEM_COUNT_PER_PAGE = 30
-
 const UserManager: FC = () => {
-  const [activePage, setPage] = useState(-1)
+  const [count, setCount] = useState(20);
+  const [skip, setSkip] = useState(0);
   const [disabled, setDisabled] = useState(false)
   const [isEditModalOpen, setIsEditModalOpen] = useState(false)
   const [activeUser, setActiveUser] = useState<UserInfoModel>({})
+  const [users, setUsers] = useState<UserInfoModel[]>([])
 
-  const { data: users, mutate } = api.admin.useAdminUsers(undefined, {
-    refreshInterval: 0,
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-  })
+  useEffect(() => {
+    api.admin.adminUsers({ count, skip })
+      .then((res) => {
+        setUsers(res.data);
+      })
+  }, [count, skip])
 
   const modals = useModals()
 
@@ -40,7 +41,7 @@ const UserManager: FC = () => {
           icon: <Icon path={mdiCheck} size={1} />,
           disallowClose: true,
         })
-        mutate(users?.filter((t) => t.id !== user.id) ?? [])
+        setUsers(users?.filter((t) => t.id !== user.id) ?? [])
       })
       .catch((err) => {
         showNotification({
@@ -68,79 +69,84 @@ const UserManager: FC = () => {
   }
 
   return (
-    <Stack>
-      <Group position="apart">
-        <Badge size="xl" radius="sm" variant="outline">
-          第 {activePage} 页
-        </Badge>
-        <Group position="right">
-          <ActionIcon
-            size="lg"
-            variant="hover"
-            disabled={activePage <= 1}
-            onClick={() => setPage(activePage - 1)}
-          >
-            <Icon path={mdiArrowLeftBold} size={1} />
-          </ActionIcon>
-          <ActionIcon
-            size="lg"
-            variant="hover"
-            disabled={users ? users.length < ITEM_COUNT_PER_PAGE : false}
-            onClick={() => setPage(activePage + 1)}
-          >
-            <Icon path={mdiArrowRightBold} size={1} />
-          </ActionIcon>
+    <Paper shadow="md" p="md">
+      <Stack>
+        <Group position="apart">
+          <Group position="left">
+            <NumberInput label="条目数量" required value={count} min={1} precision={0} onChange={(val) => setCount(val!)} />
+            <NumberInput label="偏移" required value={skip} min={0} precision={0} onChange={(val) => setSkip(val!)} />
+          </Group>
+          <Group position="right">
+            <ActionIcon
+              size="lg"
+              variant="hover"
+              disabled={skip < count}
+              onClick={() => setSkip(skip - count)}
+            >
+              <Icon path={mdiArrowLeftBold} size={1} />
+            </ActionIcon>
+            <ActionIcon
+              size="lg"
+              variant="hover"
+              disabled={users ? users.length < count : false}
+              onClick={() => setSkip(skip + count)}
+            >
+              <Icon path={mdiArrowRightBold} size={1} />
+            </ActionIcon>
+          </Group>
         </Group>
-      </Group>
-      <Table>
-        <thead>
-          <tr>
-            <th>用户名</th>
-            <th>用户角色</th>
-            <th>所拥有的队伍</th>
-            <th>激活的队伍</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users?.map((user) => (
-            <tr key={user.id}>
-              <td>{user.userName}</td>
-              <td>{user.role}</td>
-              <td>{user.ownTeamName}</td>
-              <td>{user.activeTeamName}</td>
-              <td>
-                <Group>
-                  <ActionIcon
-                    disabled={disabled}
-                    onClick={() => {
-                      setActiveUser(user)
-                      setIsEditModalOpen(true)
-                    }}
-                  >
-                    <Icon path={mdiFileEditOutline} size={1} />
-                  </ActionIcon>
-                  <ActionIcon disabled={disabled} onClick={() => onDeleteUser(user)} color="red">
-                    <Icon path={mdiDeleteOutline} size={1} />
-                  </ActionIcon>
-                </Group>
-              </td>
+        <Table>
+          <thead>
+            <tr>
+              <th>用户名</th>
+              <th>邮箱</th>
+              <th>用户角色</th>
+              <th>所拥有的队伍</th>
+              <th>激活的队伍</th>
+              <th>操作</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-      <UserEditModal
-        centered
-        size="30%"
-        title="编辑用户"
-        basicInfo={activeUser}
-        opened={isEditModalOpen}
-        onClose={() => setIsEditModalOpen(false)}
-        mutateUser={(user: UserInfoModel) => {
-          mutate([user, ...(users?.filter((n) => n.id !== user.id) ?? [])])
-        }}
-      />
-    </Stack>
+          </thead>
+          <tbody>
+            {users?.map((user) => (
+              <tr key={user.id}>
+                <td>{user.userName}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>{user.ownTeamName}</td>
+                <td>{user.activeTeamName}</td>
+                <td>
+                  <Group>
+                    <ActionIcon
+                      disabled={disabled}
+                      onClick={() => {
+                        setActiveUser(user)
+                        setIsEditModalOpen(true)
+                      }}
+                    >
+                      <Icon path={mdiFileEditOutline} size={1} />
+                    </ActionIcon>
+                    <ActionIcon disabled={disabled} onClick={() => onDeleteUser(user)} color="red">
+                      <Icon path={mdiDeleteOutline} size={1} />
+                    </ActionIcon>
+                  </Group>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <UserEditModal
+          centered
+          size="30%"
+          title="编辑用户"
+          basicInfo={activeUser}
+          opened={isEditModalOpen}
+          onClose={() => setIsEditModalOpen(false)}
+          mutateUser={(user: UserInfoModel) => {
+            setUsers([user, ...(users?.filter((n) => n.id !== user.id) ?? [])])
+          }}
+        />
+      </Stack>
+    </Paper>
   )
 }
 
