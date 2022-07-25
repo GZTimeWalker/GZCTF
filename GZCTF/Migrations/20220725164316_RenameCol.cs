9c26d85bce5ce10fa665844b2ff29d021c858054
GZTimeWalker <Time.GZ@outlook.com>
2022-07-26 00:43:54 +0800

    wip: update database

---
+++ GZCTF/Migrations/20220725164316_RenameCol.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTFServer.Migrations
+{
+    public partial class RenameCol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AcceptedUserCount",
+                table: "Challenges",
+                newName: "AcceptedCount");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AcceptedCount",
+                table: "Challenges",
+                newName: "AcceptedUserCount");
+        }
+    }
+}
