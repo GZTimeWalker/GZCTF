646f77863da88cefcf3efea63589b315681ecbb5
GZTimeWalker <Time.GZ@outlook.com>
2022-08-10 06:56:18 +0800

    deps: remove vite progress

--- GZCTF/ClientApp/vite.config.ts
+++ GZCTF/ClientApp/vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import Pages from 'vite-plugin-pages'
-import progress from 'vite-plugin-progress'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => {
@@ -38,7 +37,6 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
-      progress(),
       eslint(),
       Pages({
         dirs: [{ dir: 'src/pages', baseRoute: '' }],
