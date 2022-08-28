d913c87b42138677611d5aea939bf54fbb52db8f
GZTimeWalker <Time.GZ@outlook.com>
2022-08-28 22:44:49 +0800

    wip: add migration

---
+++ GZCTF/Migrations/20220828144352_UpdateGame.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTFServer.Migrations
+{
+    public partial class UpdateGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptWithoutReview",
+                table: "Games",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InviteCode",
+                table: "Games",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Organizations",
+                table: "Games",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Hints",
+                table: "Challenges",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "character varying(16)",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(12)",
+                oldMaxLength: 12,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptWithoutReview",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "InviteCode",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Organizations",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Hints",
+                table: "Challenges",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "character varying(12)",
+                maxLength: 12,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(16)",
+                oldMaxLength: 16,
+                oldNullable: true);
+        }
+    }
+}
