95ba85dc15adb3e29977f18b778b68c8dbe9e39d
Aether Chen <15167799+chenjunyu19@users.noreply.github.com>
2023-08-17 21:33:08 +0800

    wip: refactor InstanceEntry

--- src/GZCTF/ClientApp/src/components/InstanceEntry.tsx
+++ src/GZCTF/ClientApp/src/components/InstanceEntry.tsx
@@ -1,9 +1,20 @@
 import dayjs from 'dayjs'
 import { FC } from 'react'
-import { Stack, Text, Button, Group, Code, Tooltip, Anchor } from '@mantine/core'
+import {
+  Stack,
+  Text,
+  Button,
+  Group,
+  Tooltip,
+  Anchor,
+  TextInput,
+  ActionIcon,
+  Center,
+  Divider,
+} from '@mantine/core'
 import { useClipboard } from '@mantine/hooks'
 import { showNotification } from '@mantine/notifications'
-import { mdiCheck } from '@mdi/js'
+import { mdiCheck, mdiServerNetwork, mdiContentCopy, mdiOpenInNew, mdiOpenInApp } from '@mdi/js'
 import { Icon } from '@mdi/react'
 import { getProxyUrl } from '@Utils/Shared'
 import { useTooltipStyles } from '@Utils/ThemeOverride'
@@ -28,49 +39,86 @@ export const InstanceEntry: FC<InstanceEntryProps> = (props) => {
   const isPlatformProxy = instanceEntry.length === 36 && !instanceEntry.includes(':')
   const copyEntry = isPlatformProxy ? getProxyUrl(instanceEntry) : instanceEntry
 
+  const onCopyEntry = () => {
+    clipBoard.copy(copyEntry)
+    showNotification({
+      color: 'teal',
+      title: isPlatformProxy ? '实例入口已复制到剪贴板' : undefined,
+      message: isPlatformProxy ? '请使用客户端进行访问' : '实例入口已复制到剪贴板',
+      icon: <Icon path={mdiCheck} size={1} />,
+    })
+  }
+
+  const onOpenInNew = () => {
+    window.open(`http://${instanceEntry}`)
+  }
+
+  const onOpenInApp = () => {
+    if (!isPlatformProxy) {
+      return
+    }
+    const url = new URL('wsrx://open')
+    url.searchParams.append('url', copyEntry)
+    window.location.href = url.href
+    showNotification({
+      color: 'teal',
+      title: '已尝试拉起客户端',
+      message: '请确保客户端正确安装',
+      icon: <Icon path={mdiCheck} size={1} />,
+    })
+  }
+
   return (
-    <Stack align="flex-start" spacing={2}>
-      <Group noWrap spacing={0} w="100%" position="apart">
-        <Text size="sm" fw={600}>
-          实例入口：
-          <Tooltip label="点击复制" withArrow classNames={tooltipClasses}>
-            <Code
-              bg="transparent"
-              style={{
-                fontSize: theme.fontSizes.sm,
-                cursor: 'pointer',
-              }}
-              onClick={() => {
-                clipBoard.copy(copyEntry)
-                showNotification({
-                  color: 'teal',
-                  title: isPlatformProxy ? '实例入口已复制到剪贴板' : undefined,
-                  message: isPlatformProxy ? '请使用客户端进行访问' : '实例入口已复制到剪贴板',
-                  icon: <Icon path={mdiCheck} size={1} />,
-                })
-              }}
+    <Stack spacing={2}>
+      <TextInput
+        label={<Text fw={600}>实例入口</Text>}
+        description={
+          isPlatformProxy && (
+            <Text>
+              平台已启用代理模式，建议使用专用客户端。
+              <Anchor
+                href="https://github.com/XDSEC/WebSocketReflectorX/releases"
+                target="_blank"
+                rel="noreferrer"
+              >
+                获取客户端
+              </Anchor>
+            </Text>
+          )
+        }
+        icon={<Icon path={mdiServerNetwork} size={1} />}
+        value={copyEntry}
+        readOnly
+        styles={{
+          input: {
+            fontFamily: `${theme.fontFamilyMonospace}, ${theme.fontFamily}`,
+          },
+        }}
+        rightSection={
+          <Group spacing={2}>
+            <Divider orientation="vertical" pr={4} />
+            <Tooltip label="复制到剪贴板" withArrow classNames={tooltipClasses}>
+              <ActionIcon onClick={onCopyEntry}>
+                <Icon path={mdiContentCopy} size={1} />
+              </ActionIcon>
+            </Tooltip>
+            <Tooltip
+              label={isPlatformProxy ? '在客户端中打开' : '作为网页打开'}
+              withArrow
+              classNames={tooltipClasses}
             >
-              {instanceEntry}
-            </Code>
-          </Tooltip>
-        </Text>
+              <ActionIcon onClick={isPlatformProxy ? onOpenInApp : onOpenInNew}>
+                <Icon path={isPlatformProxy ? mdiOpenInApp : mdiOpenInNew} size={1} />
+              </ActionIcon>
+            </Tooltip>
+          </Group>
+        }
+        rightSectionWidth="5rem"
+      />
+      <Center pt="md">
         <Countdown time={context.closeTime ?? '0'} />
-      </Group>
-      {isPlatformProxy && (
-        <Group noWrap spacing={0}>
-          <Text size="sm" fw={600}>
-            获取客户端：
-            <Anchor
-              href="https://github.com/XDSEC/WebSocketReflectorX/releases"
-              target="_blank"
-              rel="noreferrer"
-            >
-              WebSocketReflectorX
-            </Anchor>
-          </Text>
-        </Group>
-      )}
-      <Group position="center" pt="md" w="100%" align="center">
+      </Center>
+      <Group position="center" pt="md" align="center">
         <Button color="orange" onClick={onProlong} disabled={instanceLeft > 10}>
           延长时间
         </Button>
