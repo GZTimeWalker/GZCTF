3abeb291a2e041309638dca6aa513e15ebf80ae9
GZTime <Time.GZ@outlook.com>
2023-10-06 21:06:59 +0800

    wip: wapper for Trans

--- src/GZCTF/ClientApp/src/utils/I18n.tsx
+++ src/GZCTF/ClientApp/src/utils/I18n.tsx
@@ -1,8 +1,11 @@
 import translation_en_US from '@Resources/strings.en-US.json'
 import translation_ja_JP from '@Resources/strings.ja-JP.json'
 import sources from '@Resources/strings.json'
-import { FC } from 'react'
-import { useTranslation as useI18nTranslation, Trans as I18nTrans } from 'react-i18next'
+import {
+  useTranslation as useI18nTranslation,
+  Trans as I18nTrans,
+  TransProps as I18nTransProps,
+} from 'react-i18next'
 
 type I18nKey = keyof typeof sources
 type Language = {
@@ -33,6 +36,4 @@ export const useTranslation = () => {
   }
 }
 
-export const Trans: FC<{ i18nKey: I18nKey }> = ({ i18nKey, ...others }) => (
-  <I18nTrans i18nKey={i18nKey} {...others} />
-)
+export const Trans = (props: I18nTransProps<I18nKey>) => <I18nTrans {...props} />
