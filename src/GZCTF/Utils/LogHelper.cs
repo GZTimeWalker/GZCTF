58baccf37a2985335e8218f31f98ebb08bb01d3c
GZTime <Time.GZ@outlook.com>
2024-02-09 00:34:56 +0800

    fix: do not store enum in log property

--- src/GZCTF/Utils/LogHelper.cs
+++ src/GZCTF/Utils/LogHelper.cs
@@ -86,7 +86,7 @@ public static class LogHelper
     {
         using (LogContext.PushProperty("UserName", uname))
         using (LogContext.PushProperty("IP", ip))
-        using (LogContext.PushProperty("Status", status))
+        using (LogContext.PushProperty("Status", status.ToString()))
         {
             logger.Log(level ?? LogLevel.Information, "{msg:l}", msg);
         }
