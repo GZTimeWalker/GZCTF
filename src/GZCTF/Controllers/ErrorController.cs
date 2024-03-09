3c6968153e5788803fd39ad6b5d3aed57bad68ea
Kengwang <github@kengwang.com.cn>
2024-03-09 12:50:49 +0800

    fix: Error Handling (#246)

---
+++ src/GZCTF/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace GZCTF.Controllers;
+
+[ApiController]
+[Route("/error")]
+public class ErrorController(IStringLocalizer<Program> localizer) : ControllerBase
+{
+    [Route("500")]
+    [ProducesResponseType(typeof(RequestResponse), StatusCodes.Status500InternalServerError)]
+    public Task<IActionResult> InternalServerError(CancellationToken cancellationToken) => Task.FromResult<IActionResult>(StatusCode(500, new RequestResponse(localizer[nameof(Resources.Program.Error_InternalServerError)], StatusCodes.Status500InternalServerError)));
+}
