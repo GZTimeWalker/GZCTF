bf4ad18d60285fbb8128500a7ecc2139a30b54e0
GZTime <Time.GZ@outlook.com>
2024-05-02 14:31:55 +0800

    fix: do not use Ulid for containers

--- src/GZCTF/Models/Data/Container.cs
+++ src/GZCTF/Models/Data/Container.cs
@@ -10,7 +10,7 @@ namespace GZCTF.Models.Data;
 public class Container
 {
     [Key]
-    public Guid Id { get; set; } = Ulid.NewUlid().ToGuid();
+    public Guid Id { get; set; }
 
     /// <summary>
     /// 镜像名称
