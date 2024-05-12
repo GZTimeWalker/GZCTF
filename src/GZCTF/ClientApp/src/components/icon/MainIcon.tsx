6171a8ef5599f1cb6a2d3ba591148063e8dbf7f9
GZTime <Time.GZ@outlook.com>
2024-05-12 12:19:27 +0800

    fix(style): icon color

--- src/GZCTF/ClientApp/src/components/icon/MainIcon.tsx
+++ src/GZCTF/ClientApp/src/components/icon/MainIcon.tsx
@@ -26,21 +26,18 @@ const MainIcon: FC<MainIconProps> = (props: MainIconProps) => {
       <g id="Flag">
         <path
           id="Flag_0"
-          // fill="#00bfa5"
           className={classes.front}
           fillRule="evenodd"
           d="M1280.55,582.029L2046.6,1224.82l-771.35,919.25L509.21,1501.28Z"
         />
         <path
           id="Flag_1"
-          // fill="#007f6e"
           className={classes.back}
           fillRule="evenodd"
           d="M1225.95,1580.54l306.42,257.11-257.12,306.42Z"
         />
         <path
           id="Flag_2"
-          // fill="#1de9b6"
           className={classes.mid}
           fillRule="evenodd"
           d="M2636.97,2699.25l-32.14,38.31-264.98,315.78L1812.4,2748.51l332.8-396.63-919.25-771.34L1997.3,661.284,3376.18,1818.3ZM1880,3601.24l0.15-.04L1351.4,4231.34,891.769,3845.67l460.361-548.64Z"
--- src/GZCTF/ClientApp/src/components/icon/WiderScreenRequiredIcon.tsx
+++ src/GZCTF/ClientApp/src/components/icon/WiderScreenRequiredIcon.tsx
@@ -10,24 +10,39 @@ const IconWiderScreenRequired: FC = () => {
       height="320"
       viewBox="0 0 6400 6400"
     >
-      <path id="arrowRodL2" fill="#007f6e" fillRule="evenodd" d="M1920,2900v600H640V2900H1920Z" />
-      <path id="arrowRodL1" fill="#00bfa5" fillRule="evenodd" d="M3200,2900v600H1920V2900H3200Z" />
+      <path
+        id="arrowRodL2"
+        className={classes.back}
+        fillRule="evenodd"
+        d="M1920,2900v600H640V2900H1920Z"
+      />
+      <path
+        id="arrowRodL1"
+        className={classes.front}
+        fillRule="evenodd"
+        d="M3200,2900v600H1920V2900H3200Z"
+      />
       <path
         id="Triangle"
         className={classes.main}
         fillRule="evenodd"
         d="M3794.48,5044.61L1305.28,3607.47V2792.53l2489.2-1437.14,705.76,407.47V4637.14Zm0.89-2875.82L2009.27,3200l1786.1,1031.21V2168.79Z"
       />
-      <path id="arrowRodR" fill="#00bfa5" fillRule="evenodd" d="M3200,3500V2900H5760v600H3200Z" />
+      <path
+        id="arrowRodR"
+        className={classes.front}
+        fillRule="evenodd"
+        d="M3200,3500V2900H5760v600H3200Z"
+      />
       <path
         id="arrowR"
-        fill="#1de9b6"
+        className={classes.mid}
         fillRule="evenodd"
         d="M6400,3200L5268.63,4331.37l-424.27-424.26L5551.47,3200l-707.11-707.11,424.27-424.26,707.11,707.11h0Z"
       />
       <path
         id="arrowL"
-        fill="#00bfa5"
+        className={classes.front}
         fillRule="evenodd"
         d="M1555.63,3907.11l-424.26,424.26L0,3200l424.264-424.26h0l707.106-707.11,424.26,424.26L848.528,3200Z"
       />
