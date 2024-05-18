133c2131b82721c94d314286ed7038c81c871e3f
GZTime <Time.GZ@outlook.com>
2024-05-18 18:07:01 +0800

    chore: allow load image and font

--- src/GZCTF/Middlewares/CspMiddleware.cs
+++ src/GZCTF/Middlewares/CspMiddleware.cs
@@ -2,8 +2,8 @@ namespace GZCTF.Middlewares;
 
 public class CspMiddleware(RequestDelegate next)
 {
-    const string CspString = "default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; " +
-        "font-src 'self' data:; object-src 'none'; frame-src 'none';";
+    const string CspString = "default-src 'self'; style-src 'self' 'unsafe-inline'; " +
+        "img-src * 'self' data:; font-src * 'self' data:; object-src 'none'; frame-src 'none';";
 
     public Task InvokeAsync(HttpContext context)
     {
