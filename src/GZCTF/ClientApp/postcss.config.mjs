d08f367337f38a23b65797779f29731f7e89040d
GZTime <Time.GZ@outlook.com>
2024-10-13 15:55:07 +0800

    fix(frontend): missing css vars may cause `NaNem` in media query

--- src/GZCTF/ClientApp/postcss.config.mjs
+++ src/GZCTF/ClientApp/postcss.config.mjs
@@ -3,5 +3,14 @@ export default {
     'postcss-preset-mantine': {
       autoRem: true,
     },
+    'postcss-simple-vars': {
+      variables: {
+        'mantine-breakpoint-xs': '36em',
+        'mantine-breakpoint-sm': '48em',
+        'mantine-breakpoint-md': '62em',
+        'mantine-breakpoint-lg': '75em',
+        'mantine-breakpoint-xl': '88em',
+      },
+    },
   },
 }
