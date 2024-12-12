a7c337e8e0e3eb768ab45a1cb02f00b945674fad
GZTime <Time.GZ@outlook.com>
2024-12-12 20:11:07 +0800

    fix(cache): remove ClientConfig cache when launch

--- src/GZCTF/Utils/PrelaunchHelper.cs
+++ src/GZCTF/Utils/PrelaunchHelper.cs
@@ -73,6 +73,7 @@ public static class PrelaunchHelper
             Program.ExitWithFatalMessage(Program.StaticLocalizer[nameof(Resources.Program.Init_InvalidCacheConfig)]);
 
         await cache.RemoveAsync(CacheKey.Index);
+        await cache.RemoveAsync(CacheKey.ClientConfig);
         await cache.RemoveAsync(CacheKey.CaptchaConfig);
     }
 
