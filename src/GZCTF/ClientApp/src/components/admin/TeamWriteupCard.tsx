c760513f8900275d4c679f2d99b27373ab8b9c84
GZTime <Time.GZ@outlook.com>
2025-09-10 00:24:07 +0800

    style: make team name in TeamWriteupCard scrolling

--- src/GZCTF/ClientApp/src/components/admin/TeamWriteupCard.tsx
+++ src/GZCTF/ClientApp/src/components/admin/TeamWriteupCard.tsx
@@ -14,6 +14,7 @@ import { Icon } from '@mdi/react'
 import dayjs from 'dayjs'
 import { FC } from 'react'
 import { Link } from 'react-router'
+import { ScrollingText } from '@Components/ScrollingText'
 import { useLanguage } from '@Utils/I18n'
 import { WriteupInfoModel } from '@Api'
 import misc from '@Styles/Misc.module.css'
@@ -31,7 +32,14 @@ export const TeamWriteupCard: FC<TeamWriteupCardProps> = ({ writeup, selected, .
   const borderColor = selected ? theme.colors[theme.primaryColor][colorScheme === 'dark' ? 8 : 6] : 'transparent'
 
   return (
-    <Card {...props} p="sm" shadow="sm" classNames={{ root: misc.hoverCard }} bd={`2px solid ${borderColor}`}>
+    <Card
+      {...props}
+      p="sm"
+      shadow="sm"
+      classNames={{ root: misc.hoverCard }}
+      bd={`2px solid ${borderColor}`}
+      data-no-move
+    >
       <Group wrap="nowrap" gap={3} justify="space-between">
         <Group gap="sm" wrap="nowrap" justify="space-between" maw="calc(100% - 2rem)">
           <Avatar alt="avatar" src={writeup.team?.avatar} size="md">
@@ -41,9 +49,7 @@ export const TeamWriteupCard: FC<TeamWriteupCardProps> = ({ writeup, selected, .
             <Text size="0.8rem" lineClamp={1} c="dimmed">
               #{writeup.team?.id}
             </Text>
-            <Text lineClamp={1} fw={600}>
-              {writeup.team?.name}
-            </Text>
+            <ScrollingText size="md" fw={600} text={writeup.team?.name ?? ''} />
             <Text size="xs" lineClamp={1} c="dimmed">
               {dayjs(writeup.uploadTimeUtc).locale(locale).format('SLL LT')}
             </Text>
